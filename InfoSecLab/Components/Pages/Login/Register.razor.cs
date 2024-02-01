3a2a3e0994a134fd4457a02e113b7c942dc07f63
Nicholas Page <npage@southhills.edu>
2024-01-31 20:42:01 -0500

    Initial commit

---
+++ InfoSecLab/Components/Pages/Login/Register.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Text.RegularExpressions;
+
+namespace InfoSecLab.Components.Pages.Login
+{
+    public partial class Register : ComponentBase
+    {
+        private const String EmailPattern = @"^[^@]+@[^@]+\.[^.]+$";
+
+        [GeneratedRegex(EmailPattern)]
+        private static partial Regex EmailRegex();
+
+        [Inject]
+        protected NavigationManager? NavigationManager { get; set; }
+
+        private String Email { get; set; } = String.Empty;
+        private bool EmailValid { get; set; } = true;
+        private String EmailErrorClass => EmailValid ? String.Empty : "is-invalid";
+
+        private String Password { get; set; } = String.Empty;
+        private String PasswordConfirmation { get; set; } = String.Empty;
+    }
+}
